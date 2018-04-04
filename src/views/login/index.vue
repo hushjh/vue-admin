b2931d9065aa8fb0957e57a801a9e6da25c4fdf6
hushjh <772074834@qq.com>
2018-04-04 08:44:08 +0800

    modify assetsPublicPath

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -80,7 +80,7 @@
                 });
               } else {
                 sessionStorage.setItem('user', JSON.stringify(user));
-                this.$router.push({ path: '/table' });
+                this.$router.push({ path: '/home' });
               }
             });
           } else {

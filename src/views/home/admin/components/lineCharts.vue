e076d34c5f1bc9819baddfdfd5d960f723a65387
hushjh <772074834@qq.com>
2018-04-09 22:13:21 +0800

    lineChart

--- src/views/home/admin/components/lineCharts.vue
+++ src/views/home/admin/components/lineCharts.vue
@@ -1,16 +1,118 @@
 <!--  -->
 <template>
-  <div></div>
+  <div ref='lineChart' :class='className' :style='{height:height,width:width}'></div>
 </template>
 
 <script>
+import echarts from 'echarts'
+require('echarts/theme/macarons') // echarts theme
 export default {
-    data () {
+  name:'lineCharts',
+  props:{
+    chartData:{
+      type:Object
+    },
+    className:{
+      type:String,
+      default:'chart'
+    },
+    height:{
+      type:String,
+      default:'350px'
+    },
+    width:{
+      type:String,
+      default:'100%'
+    }
+  },
+  data () {
     return {
+      chart:null
     };
+  },
+  beforeDestroy(){
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  mounted(){
+    this.$nextTick(()=>{
+      this.initChart();
+      this.__resizeHanlder=()=>{
+        if (this.chart) {
+          this.chart.resize()
+        }
+      }
+      window.addEventListener('resize', this.__resizeHanlder)
+    });
+  },
+  watch:{
+    chartData:{
+      deep:true,
+      handle(val){
+        this.setOption(val);
+      }
+    }
+  },
+  methods:{
+    setOption({expectedData,actualData}={}){
+      console.log('expectedData:',expectedData,'actualData:',actualData);
+      // 绘制图表
+      this.chart.setOption({
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross'
+            },
+          },
+          legend:{
+            data:['expected', 'actual']
+          },
+          xAxis: {
+              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              axisTick: {
+                show: false
+              },
+              boundaryGap: false
+          },
+          yAxis: {
+            axisTick: {
+              show: false
+            }
+          },
+          grid:{
+            left:10,
+            right:10,
+            top:20,
+            bottom:30,
+            containLabel:true
+          },
+          series: [{
+              name: 'expected',
+              type: 'line',
+              animationDuration: 2800,
+              data: expectedData
+          },{
+              name: 'actual',
+              type: 'line',
+              animationDuration: 2800,
+              data: actualData
+          }]
+      });
+    },
+    initChart(){
+      this.chart=echarts.init(this.$el,'macarons')// 基于准备好的dom，初始化echarts实例
+      this.setOption(this.chartData);
+    }
   }
 }
 
 </script>
 <style lang='css' scoped>
+.chart{
+  height:350px;
+  width:100%;
+}
 </style>
--- src/views/home/admin/index.vue
+++ src/views/home/admin/index.vue
@@ -3,8 +3,8 @@
   <div class='home-editor-container'>
     <github-corner></github-corner>
     <panel-group></panel-group>
-    <el-row>
-      <line-charts></line-charts>
+    <el-row class='chart-wrapper'>
+      <line-charts :chart-data='lineChartData'></line-charts>
     </el-row>
   </div>
 </template>
@@ -13,6 +13,24 @@
 import GithubCorner from '@/components/githubCorner'
 import PanelGroup from './components/panelGroup'
 import lineCharts from './components/lineCharts'
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
 export default {
   name:'adminHome',
   components:{
@@ -22,6 +40,7 @@ export default {
   },
   data () {
     return {
+      lineChartData:lineChartData.newVisitis
     };
   },
   mounted(){
